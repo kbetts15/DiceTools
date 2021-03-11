01216abfad06117c4a4d574c105201eac2eae68f
Kieran Betts <kieranbetts1@gmail.com>
2021-03-11 16:33:37 +0000

    Commented ConcreteProbMap
    Added class javadoc to ConcreteProbMap

--- src/ConcreteProbMap.java
+++ src/ConcreteProbMap.java
@@ -1,4 +1,10 @@
-
+/**
+ * Concrete class implementing {@link ProbMap} without adding any functionality.
+ * All keys are valid, sanitizing keys returns the same key.
+ * @author kieran
+ *
+ * @param <K>
+ */
 public class ConcreteProbMap<K> extends ProbMap<K>
 {
 	private static final long serialVersionUID = 1L;
