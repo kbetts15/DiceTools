b9e2ae38aec86a079b6aa21208bbf632d8bcb9dd
Kieran Betts <kieranbetts1@gmail.com>
2021-03-29 19:41:19 +0100

    KeyValuePair
    Implemented KeyValuePair (which extends Map.Entry<Object, Object>)

---
+++ src/diceTools/KeyValuePair.java
@@ -0,0 +1,35 @@
+package diceTools;
+
+import java.util.Map;
+
+public class KeyValuePair implements Map.Entry<Object, Object>
+{
+	private final Object key;
+	private Object value;
+	
+	public KeyValuePair(Object key, Object value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+	
+	@Override
+	public Object getKey()
+	{
+		return key;
+	}
+
+	@Override
+	public Object getValue()
+	{
+		return value;
+	}
+
+	@Override
+	public Object setValue(Object value)
+	{
+		Object oldVal = this.value;
+		this.value = value;
+		return oldVal;
+	}
+}
