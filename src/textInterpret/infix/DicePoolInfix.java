399554ae5a8d9d027c0dcf9215173edcd3651d9e
Kieran Betts <kieranbetts1@gmail.com>
2021-03-31 17:59:47 +0100

    DicePoolInfix Map arguments
    Improved DiceRollInfix to accept DiceRollMap and DicePoolMap arguments

--- src/textInterpret/infix/DicePoolInfix.java
+++ src/textInterpret/infix/DicePoolInfix.java
@@ -1,25 +1,111 @@
 package textInterpret.infix;
 
-import diceTools.DicePoolMap;
-import textInterpret.TokenFuncInputTypeException;
-import textInterpret.TokenInfix;
+import java.util.Map;
 
-public class DicePoolInfix extends TokenInfix
+import diceTools.DicePoolMap;
+import diceTools.DiceRollMap;
+
+public class DicePoolInfix extends ArgSortedInfix
 {
 
-	//TODO: implement variable number of dice and sides
+	@Override
+	public DicePoolMap operateCase(DicePoolMap a, DicePoolMap b)
+	{
+		return operateCase(a.flatten(), b.flatten());
+	}
 
 	@Override
-	public Object operate(Object objA, Object objB)
+	public DicePoolMap operateCase(DicePoolMap a, DiceRollMap b)
 	{
-		if (objA == null || objB == null)
-			throw new NullPointerException();
+		return operateCase(a.flatten(), b);
+	}
+
+	@Override
+	public DicePoolMap operateCase(DicePoolMap a, Integer b)
+	{
+		return operateCase(a.flatten(), b);
+	}
+
+	@Override
+	public DicePoolMap operateCase(DiceRollMap a, DicePoolMap b)
+	{
+		return operateCase(a, b.flatten());
+	}
+
+	@Override
+	public DicePoolMap operateCase(DiceRollMap a, DiceRollMap b)
+	{
+		DicePoolMap outPool = new DicePoolMap();
 		
-		if (!(objA instanceof Number) || !(objB instanceof Number))
-			throw new TokenFuncInputTypeException();
+		for (Map.Entry<Integer, Double> entryA : a.entrySet())
+		{
+			for (Map.Entry<Integer, Double> entryB : b.entrySet())
+			{
+				DicePoolMap rollPool = (DicePoolMap) operateCase(entryA.getKey(), entryB.getKey());
+				
+				final double normalisingFactor = entryA.getValue() * entryB.getValue();
+				DicePoolMap normalisedPool = new DicePoolMap();
+				
+				rollPool.forEach((key, value) -> {normalisedPool.put(key, value * normalisingFactor);});
+				
+				outPool.mergeAll(normalisedPool);
+			}
+		}
 		
-		int numDice = ((Number) objA).intValue();
-		int numSides = ((Number) objB).intValue();
+		return outPool;
+	}
+
+	@Override
+	public DicePoolMap operateCase(DiceRollMap a, Integer b)
+	{
+		DicePoolMap outPool = new DicePoolMap();
+		
+		for (Map.Entry<Integer, Double> entry : a.entrySet())
+		{
+			DicePoolMap rollPool = (DicePoolMap) operateCase(entry.getKey(), b);
+			
+			final double normalisingFactor = entry.getValue();
+			DicePoolMap normalisedPool = new DicePoolMap();
+			
+			rollPool.forEach((key, value) -> {normalisedPool.put(key, value * normalisingFactor);});
+			
+			outPool.mergeAll(normalisedPool);
+		}
+		
+		return outPool;
+	}
+
+	@Override
+	public DicePoolMap operateCase(Integer a, DicePoolMap b)
+	{
+		return operateCase(a, b.flatten());
+	}
+
+	@Override
+	public DicePoolMap operateCase(Integer a, DiceRollMap b)
+	{
+		DicePoolMap outPool = new DicePoolMap();
+		
+		for (Map.Entry<Integer, Double> entry: b.entrySet())
+		{
+			DicePoolMap rollPool = (DicePoolMap) operateCase(a, entry.getKey());
+
+			final double normalisingFactor = entry.getValue();
+			DicePoolMap normalisedPool = new DicePoolMap();
+			
+			rollPool.forEach((key, value) -> {normalisedPool.put(key, value * normalisingFactor);});
+			
+			outPool.mergeAll(normalisedPool);
+		}
+		
+		return outPool;
+	}
+
+	@Override
+	public DicePoolMap operateCase(Integer a, Integer b)
+	{
+		int numDice = ((Number) a).intValue();
+		int numSides = ((Number) b).intValue();
 		
 		return DicePoolMap.diceRoll(numDice, numSides);
 	}
