0d557a44c678041d7481798ff20babe4026fa6a8
Kieran Betts <kieranbetts1@gmail.com>
2021-04-06 14:56:27 +0100

    StringIterable JavaDoc
    Wrote JavaDoc comments for StringIterable

--- src/textInterpret/StringIterable.java
+++ src/textInterpret/StringIterable.java
@@ -3,10 +3,22 @@ package textInterpret;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Provides an <code>Iterable</code> view of a <code>String</code> with which to iterate characters
+ * 
+ * @author kieran
+ */
 public class StringIterable implements Iterable<Character>
 {
+	/**
+	 * <code>String</code> for which the <code>StringIterable</code> provides character iterators
+	 */
 	private final String s;
 	
+	/**
+	 * Instantiate a {@link StringIterable}
+	 * @param s		<code>String</code> for which the <code>StringIterable</code> will provide character iterators
+	 */
 	public StringIterable(String s)
 	{
 		this.s = s;
@@ -18,8 +30,19 @@ public class StringIterable implements Iterable<Character>
 		return new StringIterator();
 	}
 	
+	/**
+	 * Iterates the characters of a <code>String</code>
+	 * 
+	 * @author kieran
+	 */
 	private class StringIterator implements Iterator<Character>
 	{
+		/**
+		 * Current position in the <code>String</code>.
+		 * This is equivalent to the position of the next
+		 * <code>Character</code> to be returned by
+		 * {@link StringIterator#next next()}
+		 */
 		private int pos = 0;
 		
 		@Override
