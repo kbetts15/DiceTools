bca5909ade2d2836dab3742aab8cebade833c9ae
Kieran Betts <kieranbetts1@gmail.com>
2021-07-03 09:29:56 +0100

    Commit JavaDoc
    Commit generated JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"diceTools"},{"l":"diceTools.function"},{"l":"textInterpret"},{"l":"textInterpret.function"},{"l":"textInterpret.infix"},{"l":"textInterpret.unary"}]
